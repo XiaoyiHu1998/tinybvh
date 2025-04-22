1a48c60f6879a62f3d2ab1a2842564d710108555
Xiao yi Hu <xy1998@live.nl>
2025-04-23 01:16:34 +0200

    Added premake5.lua to make tinybvh build into a static library for Hazel

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "tinybvh"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"tiny_bvh.h",
+		"tiny_ocl.h",
+		"tiny_bvh_lib.cpp",
+	}
+
+	includedirs
+	{
+		"external/OpenCL/inc"
+	}
+
+	links
+	{
+		"external/OpenCL/lib/OpenCL.lib"
+	}
+    
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
